a6aa326ee8cdf7745d2f6c04c914d70096d0278d
yyy <super.doter.yyy@gmail.com>
2015-08-16 17:43:36 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp");
-var GulpH5Ejs = require("h5-ejs"); 
+var GulpH5Ejs = require("./index.js"); 
 var gulpH5Ejs = new GulpH5Ejs();
 gulp.task('test', function(cb) {
   gulp.src([ "handlebar" + "/**/*.html"])
